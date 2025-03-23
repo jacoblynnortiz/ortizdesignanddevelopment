0cbfa739e5702e2c69bef216c260785c46f65b71
Jacob Ortiz <98186544+jacoblynnortiz@users.noreply.github.com>
2025-03-23 03:21:32 -0500

    Delete app directory

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-$.getJSON('app/js/metaInformation.json', function (metaInfomration) {
-    let copyrightHeader = document.getElementById('copyrightHeader');
-
-    for (let i = 0; i < metaInfomration.length; i++) {
-        copyrightHeader.innerText = metaInfomration[i].copyright;
-    }
-});
